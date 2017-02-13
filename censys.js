8ac78761058a9dd33c99fbe494a6e229ec8ced68
BrianLee <briankigondu@gmail.com>
2017-02-13 21:42:54 +0300

    Fixed a few errors

--- censys.js
+++ censys.js
@@ -5,22 +5,26 @@ check if function holds up for all kinds of search variations
 check what other functions from the other apis do and what they return and get what they do
 */
 
-var config = require('./config');
-var request = require('./request');
+var config = require('./censys_config');
+var request = require('request');
 
 module.exports = {
 	search:function(index, searchTerm){
 		var url = "https://www.censys.io/api/v1/search/"+index+"/"+searchTerm;
 		request.get(url,{
   		'auth':{
-		    'user':config.api_key,
-		    'pass':config.api_secret
+		    'user':"92d6ccbf-ad24-4326-9ccc-43af301eebd4",
+		    'pass':"8lr2dHV6T2AleMZwEs0ShHRXdGPXQbms"
 			}
 		}).on('response', function(response){
 			//search function returns the response
+			console.log(response);
 			return response;
 		}).on('error', function(err){
 			console.error(err);
 		})	
+	},
+	export:function(){
+
 	}
 }
